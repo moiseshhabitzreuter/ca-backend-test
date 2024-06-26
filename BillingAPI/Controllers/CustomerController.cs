0549ea66aefa200035cb28f93430289521456b14
Moisés Henrique Habitzreuter <moisesblu3@gmail.com>
2024-06-26 16:50:54 -0300

    added Customer and Product Controllers with all CRUD operations

---
+++ BillingAPI/Controllers/CustomerController.cs
@@ -0,0 +1,71 @@
+﻿using BillingAPI.Data;
+using BillingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace BillingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly MongoDbContext _context;
+
+        public CustomerController(MongoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetCustomer")]
+        public IActionResult GetCustomer(string id)
+        {
+            var customer = _context.Customers.Find<Customer>(customer => customer.Id == id && !customer.IsDeleted).FirstOrDefault();
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer([FromBody] Customer customer)
+        {
+            _context.Customers.InsertOne(customer);
+            return CreatedAtRoute("GetCustomer", new { id = customer.Id.ToString() }, customer);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult UpdateCustomer(string id, [FromBody] Customer customerIn)
+        {
+            var customer = _context.Customers.Find<Customer>(customer => customer.Id == id && !customer.IsDeleted).FirstOrDefault();
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            //Trocar ReplaceOne por Update
+            _context.Customers.ReplaceOne(customer => customer.Id == id, customerIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult DeleteCustomer(string id)
+        {
+            var customerToDelete = _context.Customers.Find<Customer>(customer => customer.Id == id && !customer.IsDeleted).FirstOrDefault();
+
+            if (customerToDelete == null)
+            {
+                return NotFound();
+            }
+            //Trocar ReplaceOne por Update
+            customerToDelete.IsDeleted = true;
+            _context.Customers.ReplaceOne(customer => customer.Id == id, customerToDelete);
+
+            return NoContent();
+        }
+    }
+}
---
+++ BillingAPI/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using BillingAPI.Data;
+using BillingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace BillingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly MongoDbContext _context;
+
+        public ProductController(MongoDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        public IActionResult GetProduct(string id)
+        {
+            var product = _context.Products.Find<Product>(product => product.Id == id && !product.IsDeleted).FirstOrDefault();
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody] Product product)
+        {
+            _context.Products.InsertOne(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id.ToString() }, product);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult UpdateProduct(string id, [FromBody] Product productIn)
+        {
+            var product = _context.Products.Find<Product>(product => product.Id == id && !product.IsDeleted).FirstOrDefault();
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            //Trocar ReplaceOne por Update
+            _context.Products.ReplaceOne(product => product.Id == id, productIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult DeleteProduct(string id)
+        {
+            var productToDelete = _context.Products.Find<Product>(product => product.Id == id && !product.IsDeleted).FirstOrDefault();
+
+            if (productToDelete == null)
+            {
+                return NotFound();
+            }
+            //Trocar ReplaceOne por Update
+            productToDelete.IsDeleted = true;
+            _context.Products.ReplaceOne(product => product.Id == id, productToDelete);
+
+            return NoContent();
+        }
+    }
+}
